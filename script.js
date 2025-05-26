a33b1f1e7efa22cb4b41289ff3b7d5c7c25db600
Circuit-Master <loganhumeniuk@gmail.com>
2025-05-25 17:42:46 -0700

    rolled back 1 change

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const reflectionBox = document.getElementById("reflection-box");
 const refreshButton = document.getElementById("refresh-btn");
 
 // Correct REST endpoint
-const HF_API = "https://circuit-master-eternal-torment.hf.space/run/predict";
+const HF_API = "https://circuit-master-eternal-torment.hf.space/api/predict/";
 
 async function getReflection() {
   reflectionBox.innerText = "Thinking...";
